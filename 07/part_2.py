4b6a757e514f22b9731d09c703ad663bb00cc104
ThatOtherAndrew <stroev.andrew@gmail.com>
2024-12-07 05:47:48 +0000

    ⏩ Speed up day 7 part 2 with multiprocessing

--- 07/part_2.py
+++ 07/part_2.py
@@ -1,7 +1,25 @@
 import itertools
+from concurrent.futures.process import ProcessPoolExecutor
 from math import log
 
 from tqdm import tqdm
+
+
+def test_equation(target: int, operands: list[int]) -> int:
+    for operators in itertools.product('+*|', repeat=len(operands) - 1):
+        calculation = operands[0]
+        for op, num in zip(operators, operands[1:]):
+            if op == '+':
+                calculation += num
+            elif op == '*':
+                calculation *= num
+            else:
+                calculation = calculation * (10 ** int(log(num, 10) + 1)) + num
+
+        if calculation == target:
+            return target
+
+    return 0
 
 
 def main():
@@ -11,23 +29,10 @@ def main():
             raw_target, raw_operands = line.split(': ')
             equations.append((int(raw_target), list(map(int, raw_operands.split()))))
 
-    total = 0
-    for target, operands in tqdm(equations):
-        for operators in itertools.product('+*|', repeat=len(operands) - 1):
-            calculation = operands[0]
-            for op, num in zip(operators, operands[1:]):
-                if op == '+':
-                    calculation += num
-                elif op == '*':
-                    calculation *= num
-                else:
-                    calculation = calculation * (10 ** int(log(num, 10) + 1)) + num
+    with ProcessPoolExecutor() as executor:
+        tested_equations = list(tqdm(executor.map(test_equation, *zip(*equations)), total=len(equations)))
 
-            if calculation == target:
-                total += calculation
-                break
-
-    print(total)
+    print(sum(tested_equations))
 
 if __name__ == '__main__':
     main()
